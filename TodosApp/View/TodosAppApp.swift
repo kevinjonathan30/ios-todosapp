d9096be9d940e136fb85079cdbfc9aa0047d2723
KevinJonathan30 <kevinjonathan3010@gmail.com>
2024-01-30 12:22:01 +0700

    first commit

--- TodosApp/View/TodosAppApp.swift
+++ TodosApp/View/TodosAppApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct TodosAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                HomeView()
+            }
         }
     }
 }
